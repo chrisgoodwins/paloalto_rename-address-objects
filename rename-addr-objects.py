945f1c96a41368ba590c8ad80e346e6567292679
Chris Goodwin <chrisgoodwins@gmail.com>
2020-03-14 15:57:06 -0700

    Update rename-addr-objects.py

--- rename-addr-objects.py
+++ rename-addr-objects.py
@@ -37,31 +37,31 @@ except ImportError:
 ###############################################################################
 
 
-# Prompts the user to enter the IP/FQDN of a firewall to retrieve the api key
+# Prompts the user to enter an address, then checks it's validity
 def getfwipfqdn():
     while True:
-        fwipraw = input("\nPlease enter Panorama/firewall IP or FQDN: ")
-        ipr = re.match(r"^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", fwipraw)
-        fqdnr = re.match(r"(?=^.{4,253}$)(^((?!-)[a-zA-Z0-9-]{1,63}(?<!-)\.)+[a-zA-Z]{2,63}$)", fwipraw)
+        fwipraw = input('\nPlease enter Panorama/firewall IP or FQDN: ')
+        ipr = re.match(r'^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$', fwipraw)
+        fqdnr = re.match(r'(?=^.{4,253}$)(^((?!-)[a-zA-Z0-9-]{1,63}(?<!-)\.)+[a-zA-Z]{2,63}$)', fwipraw)
         if ipr:
             break
         elif fqdnr:
             break
         else:
-            print("\nThere was something wrong with your entry. Please try again...\n")
+            print('\nThere was something wrong with your entry. Please try again...\n')
     return fwipraw
 
 
 # Prompts the user to enter a username and password
 def getCreds():
     while True:
-        username = input("Please enter your user name: ")
-        usernamer = re.match(r"^[\w-]{3,24}$", username)
+        username = input('Please enter your user name: ')
+        usernamer = re.match(r'^[\w-]{3,24}$', username)
         if usernamer:
-            password = getpass.getpass("Please enter your password: ")
+            password = getpass.getpass('Please enter your password: ')
             break
         else:
-            print("\nThere was something wrong with your entry. Please try again...\n")
+            print('\nThere was something wrong with your entry. Please try again...\n')
     return username, password
 
 
@@ -69,25 +69,24 @@ def getCreds():
 def getkey(fwip):
     while True:
         try:
-            fwipgetkey = fwip
             username, password = getCreds()
-            keycall = "https://%s/api/?type=keygen&user=%s&password=%s" % (fwipgetkey, username, password)
+            keycall = f'https://{fwip}/api/?type=keygen&user={username}&password={password}'
             r = requests.get(keycall, verify=False)
             tree = ET.fromstring(r.text)
-            if tree.get('status') == "success":
-                apikey = tree[0][0].text
+            if tree.get('status') == 'success':
+                apikey = tree.find('./result/key').text
                 break
             else:
-                print("\nYou have entered an incorrect username or password. Please try again...\n")
+                print('\nYou have entered an incorrect username or password. Please try again...\n')
         except requests.exceptions.ConnectionError:
-            print("\nThere was a problem connecting to the firewall. Please check the address and try again...\n")
+            print('\nThere was a problem connecting to the firewall. Please check the address and try again...\n')
             exit()
     return apikey
 
 
 # Presents the user with a choice of device-groups
 def getDG(fwip, mainkey):
-    dgXmlUrl = "https://%s/api/?type=config&action=get&xpath=/config/devices/entry/device-group&key=%s" % (fwip, mainkey)
+    dgXmlUrl = f"https://{fwip}/api/?type=config&action=get&xpath=/config/devices/entry/device-group&key={mainkey}"
     r = requests.get(dgXmlUrl, verify=False)
     devTree = ET.fromstring(r.text)
     devTreeString = './/device-group/entry'
@@ -99,7 +98,7 @@ def getDG(fwip, mainkey):
             print('\n\nHere\'s a list of device groups found in Panorama...\n')
             i = 1
             for dgName in dgList:
-                print('%s) %s' % (i, dgName))
+                print(f'{i}) {dgName}')
                 i += 1
             dgChoice = int(input('\nChoose a number for the device-group:\n\nAnswer is: '))
             print('\n')
@@ -114,7 +113,7 @@ def getDG(fwip, mainkey):
 
 # Determines whether the device is Panorama or firewall
 def getDevType(fwip, mainkey):
-    devURL = "https://%s/api/?type=config&action=get&xpath=/config/devices/entry/device-group&key=%s" % (fwip, mainkey)
+    devURL = f"https://{fwip}/api/?type=config&action=get&xpath=/config/devices/entry/device-group&key={mainkey}"
     r = requests.get(devURL, verify=False)
     devTree = ET.fromstring(r.text)
     if devTree.find('.//device-group/entry') is None:
@@ -130,12 +129,12 @@ def getDevType(fwip, mainkey):
 # Returns a list of address objects from firewall or Panorama DG
 def getAddressObjects(fwip, mainkey, dg):
     if dg is None:
-        devURL = "https://%s/api/?type=config&action=get&xpath=/config/devices/entry/vsys/entry/address&key=%s" % (fwip, mainkey)
+        devURL = f"https://{fwip}/api/?type=config&action=get&xpath=/config/devices/entry/vsys/entry/address&key={mainkey}"
         r = requests.get(devURL, verify=False)
         devTree = ET.fromstring(r.text)
         addrList = devTree.findall('.//address/entry')
     else:
-        devURL = "https://%s/api/?type=config&action=get&xpath=/config/devices/entry/device-group/entry[@name='%s']/address&key=%s" % (fwip, dg, mainkey)
+        devURL = f"https://{fwip}/api/?type=config&action=get&xpath=/config/devices/entry/device-group/entry[@name='{dg}']/address&key={mainkey}"
         r = requests.get(devURL, verify=False)
         devTree = ET.fromstring(r.text)
         addrList = devTree.findall('.//address/entry')
@@ -161,17 +160,17 @@ def matchAddressObjects(fwip, mainkey, dg, addrList_mods):
                 continue
             if ip_original == ip_new:
                 if name_original == name_new:
-                    print('***** Match found: PAN object: %s: %s\n   -- No change required, since the name already matches the list entry' % (name_original, ip_new))
+                    print(f'***** Match found: PAN object: {name_original}: {ip_new}\n   -- No change required, since the name already matches the list entry')
                     addrList_currentMatch.append((name_original, name_new, ip_new))
                     continue
                 else:
-                    print('***** Match found: PAN object: %s: %s\n                -- Changed to: %s: %s' % (name_original, ip_new, name_new, ip_new))
+                    print(f'***** Match found: PAN object: {name_original}: {ip_new}\n                -- Changed to: {name_new}: {ip_new}')
                 for entry in addrList_names + addrList_currentMatch:
-                    if addrList_names + addrList_currentMatch == []:
+                    if not addrList_names + addrList_currentMatch:
                         break
                     if ip_new == entry[2] and name_new == entry[1]:
-                        print(' ^^-- There was a duplicate - %s: %s - Already in use...\n      The duplicate object will be named %s_DUPLICATE_%s\n' % (entry[1], entry[2], entry[1], str(count)))
-                        name_new = name_new + '_DUPLICATE_' + str(count)
+                        print(f' ^^-- There was a duplicate - {entry[1]}: {entry[2]} - Already in use...\n      The duplicate object will be named {entry[1]}_DUPLICATE_{str(count)}\n')
+                        name_new += f'_DUPLICATE_{str(count)}'
                         count += 1
                         break
                 addrList_names.append((name_original, name_new, ip_new))
@@ -180,21 +179,21 @@ def matchAddressObjects(fwip, mainkey, dg, addrList_mods):
 
 # Pushes the API calls to firewall or Panorama
 def pushAddrChanges(fwip, mainkey, dg, addrList_names):
-    if addrList_names == []:
+    if not addrList_names:
         print('\n\nThere were no matches between the list provided and the set of address objects on the Panorama/firewall, or there were no changes required\n\n')
     else:
         input('\n\n\nPress Enter to push API calls to Panorama/firewall (or CTRL+C to kill the script)... \n\n\n')
         for entry in addrList_names:
             if dg is None:
-                devURL = "https://%s/api/?type=config&action=rename&xpath=/config/devices/entry/vsys/entry/address/entry[@name='%s']&newname=%s&key=%s" % (fwip, entry[0], entry[1], mainkey)
+                devURL = f"https://{fwip}/api/?type=config&action=rename&xpath=/config/devices/entry/vsys/entry/address/entry[@name='{entry[0]}']&newname={entry[1]}&key={mainkey}"
             else:
-                devURL = "https://%s/api/?type=config&action=rename&xpath=/config/devices/entry/device-group/entry[@name='%s']/address/entry[@name='%s']&newname=%s&key=%s" % (fwip, dg, entry[0], entry[1], mainkey)
+                devURL = f"https://{fwip}/api/?type=config&action=rename&xpath=/config/devices/entry/device-group/entry[@name='{dg}']/address/entry[@name='{entry[0]}']&newname={entry[1]}&key={mainkey}"
             r = requests.get(devURL, verify=False)
             tree = ET.fromstring(r.text)
             if tree.get('status') == 'success':
-                print('API push for ' + entry[0] + ' change to ' + entry[1] + ' was successful')
+                print(f'API push for {entry[0]} change to {entry[1]} was successful')
             else:
-                print('***** API push error for ' + entry[0] + ' change to ' + entry[1] + ' *****')
+                print(f'***** API push error for {entry[0]} change to {entry[1]} *****')
 
 
 def main():
@@ -212,12 +211,12 @@ def main():
             dg = getDG(fwip, mainkey)
             matchAddressObjects(fwip, mainkey, dg, addrList_mods)
             dgChoice = input('\n\nWould you like to run this script against another device group? [Y/n]  ')
-            if dgChoice == '' or dgChoice == 'Y' or dgChoice == 'y':
+            if not dgChoice or dgChoice.lower() == 'y':
                 newList = input('\n\nEnter the name of the address list for this device group, (leave blank to use the same list): ')
-                if newList != '':
+                if newList:
                     addrList_mods = [tuple(line.rstrip().split(',')) for line in open(newList)]
                 continue
-            elif dgChoice == 'N' or dgChoice == 'n':
+            elif dgChoice.lower() == 'n':
                 break
             else:
                 print("\n\nChoose '1' or '2', try again...\n")
